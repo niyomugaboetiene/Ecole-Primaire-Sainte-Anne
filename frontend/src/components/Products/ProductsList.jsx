c065326924d417f18b35e170f2f82e0d1b3de567
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-28 17:35:40 +0200

    First commit of EPSA(frontend based)

--- frontend/src/components/Products/ProductsList.jsx
+++ frontend/src/components/Products/ProductsList.jsx
@@ -8,6 +8,38 @@ const ProductList = () => {
     const handleGetProducts = async () => {
        try {
            const res = await axios.get('http://localhost:5000/products/list')
+
+           setProducts(res.data.list);
+       } catch (err) {
+        console.error(err);
        }
     }
+
+    useEffect(() => {
+        handleGetProducts();
+    }, []);
+
+    return (
+        <div>
+            <div>
+                <table border={2}>
+                    <thead>
+                        <tr>
+                            <th>Product Id</th>
+                            <th>Product Product Name</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {products?.map((prod, index) => (
+                            <tr key={index}>
+                                <td>{prod.Product_Id}</td>
+                                <td>{prod.Product_Name}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 }
