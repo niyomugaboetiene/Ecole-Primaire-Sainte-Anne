c1b54bbdc1af4934cca39dc3972848fda2e8044e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-28 14:28:59 +0200

    First commit of EPSA

--- backend/schema/ProductSchema.js
+++ backend/schema/ProductSchema.js
@@ -1,2 +1,10 @@
 import mongoose from "mongoose";
 
+const ProductSchema = mongoose.Schema({
+    Product_Id: { type: mongoose.Schema.Types.ObjectId },
+    Product_Name: { type: String, required: true }
+});
+
+const Product = mongoose.model("products", ProductSchema);
+
+export default Product;
