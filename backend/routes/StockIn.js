abf4c851683c24355213827a3a1c07b3ab17b31a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-28 15:18:56 +0200

    First commit of EPSA

--- backend/routes/StockIn.js
+++ backend/routes/StockIn.js
@@ -41,15 +41,15 @@ router.get('/single/:_id', async (req, res) => {
         const { _id } = req.params;
 
         if (!_id) {
-            return res.status(404).json({ message: 'Product Id is required' });
+            return res.status(404).json({ message: 'Stock Id is required' });
         }
         const List = await Stock_In.findById(_id);
 
         if (List.length === 0) {
-            return res.status(404).json({ message: 'No product in the system' });
+            return res.status(404).json({ message: 'No stock in in the system' });
         } 
 
-        return res.status(200).json({ message: 'Product list', list: List });
+        return res.status(200).json({ message: 'Stock list', list: List });
     } catch (err) {
         console.error(err);
         return res.status(500).json({ message: 'Internal server error' });
@@ -58,21 +58,26 @@ router.get('/single/:_id', async (req, res) => {
 
 
 router.put('/update/:_id', async (req, res) => {
-    try {
-        const _id = req.params._id;
+  try {
+       const  {Product_Id, Date, Quantity, Unit_price }  = req.body;
+  
+       let receivedFields = {};
 
-        if (!_id) return res.status(400).json({ message: 'FIll out IDs' });
-        const { Product_Name } = req.body;
+       if (Product_Id) receivedFields.Product_Id = Product_Id;
+       if (Date) receivedFields.Date = Date;
+       if (Quantity) receivedFields.Quantity = Quantity;
+       if (Unit_price) receivedFields.Unit_price = Unit_price;
 
-        let newFields = {}
-        if (Product_Name) newFields.Product_Name = Product_Name;
+       let Total_price;
+       if (Quantity || Unit_price) {
+            Total_price = Quantity * Unit_price;
+       }
+        const StockIN = await Stock_In.create({ Product_Id, Date, Quantity, Unit_price, Total_price  });
 
-        const updatedProduct = await Stock_In.findByIdAndUpdate(_id, newFields, { new: true });
-
-        return res.status(200).json({ message: 'Updated successfully', new: updatedProduct });
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({ message: 'Intenal server error' });
+        return res.status(201).json({ message: 'New Stock In added successfully', stockIn: StockIN });
+     } catch (err) {
+         console.error(err);
+         return res.status(500).json({ message: 'Internal server error' });
     }
 });
 
