17890b846a768dcaa96785d7119ed826f6a59148
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-28 18:19:59 +0200

    First commit of EPSA

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import AddProduct from "./components/Products/AddProduct";
 import ProductList from "./components/Products/ProductsList";
+import UpdateProduct from "./components/Products/UpdateProduct";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
 
@@ -8,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/products/AddNew" element={<AddProduct />}/>
           <Route path="/products/list" element={<ProductList />}/>
+          <Route path="/products/update/:_id" element={<UpdateProduct /> }/>
         </Routes>
      </BrowserRouter>
   )
--- frontend/src/components/Products/UpdateProduct.jsx
+++ frontend/src/components/Products/UpdateProduct.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import axios from "axios";
 import { FaEdit } from "react-icons/fa";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
+
 
 const UpdateProduct = () => {
     const [Product_Id, setProduct_Id] = useState(0);
@@ -10,6 +11,7 @@ const UpdateProduct = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [message, setMessage] = useState("");
     const [error, setError] = useState("");
+    const navigate = useNavigate();
 
     const { _id } = useParams();
 
@@ -32,9 +34,12 @@ const UpdateProduct = () => {
     const handleUpdateProduct = async () => {
         try {
             setIsLoading(true);
-            const res = await axios.post(`http://localhost:5000/products/update/${_id}`, { Product_Id, Product_Name });
+            const res = await axios.put(`http://localhost:5000/products/update/${_id}`, { Product_Id, Product_Name });
             setMessage(res.data.message);
             setError("");
+            setTimeout(() => {
+              navigate('/products/list');
+            }, 2000);
         } catch (err) {
             const errorMessage = err.response?.data?.message || "Error occured";
             console.error(err);
@@ -63,16 +68,18 @@ const UpdateProduct = () => {
                 <div className="mt-2">
                     <label htmlFor="" className="text-gray-800 text-lg block">Product Id</label>
                     <input type="text" 
-                    className="w-full bg-sky-100 py-2 p-1 rounded-full mt-1 focus:outline-1 focus:outline-sky-300"
-                    placeholder="Enter Product Id (unique)"
-                     onChange={(e) => setProduct_Id(e.target.value)} />
+                       value={Product_Id} disabled
+                       className="w-full bg-sky-100 py-2 p-1 rounded-full mt-1 focus:outline-1 focus:outline-sky-300"
+                       placeholder="Enter Product Id (unique)"
+                        onChange={(e) => setProduct_Id(e.target.value)} />
                 </div>
                 <div className="mt-2">
                  <label htmlFor="" className="text-gray-800 text-lg block">Product Name</label>
                  <input type="text" 
-                 placeholder="Enter Product Name" 
-                 className="w-full bg-sky-100 py-2 p-1 rounded-full mt-1 focus:outline-1 focus:outline-sky-300"
-                 onChange={(e) => setProduct_Name(e.target.value)} />
+                    value={Product_Name}
+                    placeholder="Enter Product Name" 
+                    className="w-full bg-sky-100 py-2 p-1 rounded-full mt-1 focus:outline-1 focus:outline-sky-300"
+                    onChange={(e) => setProduct_Name(e.target.value)} />
                 </div>
 
                 <button onClick={handleUpdateProduct} className="w-full flex justify-center items-center  mt-6 bg-sky-300 py-2 gap-3 text-white font-bold rounded-full hover:bg-sky-400 transition-colors"><FaEdit />Update</button>
