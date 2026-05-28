19990d2aa4887d64beedfe47b173c86e9dba9f8e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-28 18:24:53 +0200

    First commit of EPSA

--- frontend/src/components/Stock_in/AddStock_in.jsx
+++ frontend/src/components/Stock_in/AddStock_in.jsx
@@ -8,4 +8,29 @@ const AddStockIn = () => {
     const [Date, setDate] = useState(null);
     const [Quantity, setQuantity] = useState(0);
     const [Unit_price, setUnit_price] = useState(0);
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    
+    const [products, setProducts] = useState(null);
+
+    const handleGetProducts = async () => {
+       try {
+           const res = await axios.get('http://localhost:5000/products/list')
+
+           setProducts(res.data.list);
+       } catch (err) {
+        console.error(err);
+       }
+    }
+
+    useEffect(() => {
+        handleGetProducts();
+    }, []);
+
+    const handleAddStock = async () => {
+        try {
+            
+        }
+    }
 }
