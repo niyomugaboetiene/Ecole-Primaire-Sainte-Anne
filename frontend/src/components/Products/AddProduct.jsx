df0ace4eb79ef33b4dfff6a47775807397cf932f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-28 17:19:32 +0200

    First commit of EPSA(frontend based)

--- frontend/src/components/Products/AddProduct.jsx
+++ frontend/src/components/Products/AddProduct.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
+import { FaPlus, FaProductHunt} from "react-icons/fa";
+import { SiIfood} from "react-icons/si";
 
 const AddProduct = () => {
     const [Product_Id, setProduct_Id] = useState(0);
