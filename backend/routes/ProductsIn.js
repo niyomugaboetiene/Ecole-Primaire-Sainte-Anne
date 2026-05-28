973a63e4e1dcd53bbd7c233217f3799e56b9c5aa
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-28 14:43:43 +0200

    First commit of EPSA

--- backend/routes/ProductsIn.js
+++ backend/routes/ProductsIn.js
@@ -16,6 +16,41 @@ router.post('/AddNew', async (req, res) => {
     return res.status(201).json({ message: 'New product added successfully', product: newProduct });
 } catch (err) {
     console.error(err);
+    return res.status(500).json({ message: 'Internal server error' });
 }
+});
 
-}) 
+router.get('/list', async (req, res) => {
+    try {
+        const List = await Product.find();
+
+        if (List.length === 0) {
+            return res.status(404).json({ message: 'No product in the system' });
+        } 
+
+        return res.status(200).json({ message: 'Product list', list: List });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+router.get('/single/:Product_Id', async (req, res) => {
+    try {
+        const { Product_Id } = req.body;
+
+        if (!Product_Id) {
+            return res.status(404).json({ message: 'Product Id is required' });
+        }
+        const List = await Product.findById(Product_Id);
+
+        if (List.length === 0) {
+            return res.status(404).json({ message: 'No product in the system' });
+        } 
+
+        return res.status(200).json({ message: 'Product list', list: List });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
