f2f1b6f4c9e8cbd2079d18187e7ab4f88a950579
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-28 19:27:30 +0200

    First commit of EPSA(frontend baed)

---
+++ frontend/src/components/Stock_out/AddStockOut.jsx
@@ -0,0 +1,99 @@
+import { useState, useEffect } from "react";
+import { FaPlus } from "react-icons/fa";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const AddStockOut = () => {
+    // Product_Id, Date, Quantity, Unit_price
+    const [Product_Id, setProduct_Id] = useState(null);
+    const [Date, setDate] = useState(null);
+    const [Quantity, setQuantity] = useState(0);
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+    
+    const [products, setProducts] = useState(null);
+
+    const handleGetProducts = async () => {
+       try {
+           const res = await axios.get('http://localhost:5000/products/list')
+
+           setProducts(res.data.list);
+       } catch (err) {
+        console.error(err);
+       }
+    }
+
+    useEffect(() => {
+        handleGetProducts();
+    }, []);
+
+    const handleAddStockOut = async () => {
+        try {
+            setLoading(true);
+            const res = await axios.post('http://localhost:5000/stockOut/AddNew', { Product_Id, Date, Quantity });
+            setMessage(res.data.message);
+            setLoading(false);
+            setTimeout(() => {
+                navigate('/stockOut/list');
+            }, 2000);
+        } catch (err) {
+            console.error(err);
+            const errorMessage = err.response?.data?.message || "Error occured";
+            setError(errorMessage);
+        }
+    }
+
+    return (
+        <div className="bg-sky-200 min-h-screen flex justify-center items-center ">
+            <div className="bg-white p-3 h-fit w-100 rounded-lg shadow-lg">
+                <div>
+                    {message && (
+                        <div className="bg-green-200 py-2 rounded-lg p-2 font-bold text-green-600 mb-3">
+                            {message}
+                        </div>
+                    )}
+                    {error && (
+                        <div className="bg-red-200 py-2 rounded-lg p-2 font-bold text-red-600 mb-3">
+                            {error}
+                        </div>
+                    )}
+                </div>
+
+                    <h1 className="text-center text-gray-700 font-bold text-lg">Add Stock Out</h1>
+                <div className="mt-2">
+                    <label htmlFor="" className="text-gray-800 text-lg block">Products</label>
+                    <select 
+                       className="w-full bg-sky-100 py-2 p-1 rounded-full mt-1 focus:outline-1 focus:outline-sky-300"
+                       onChange={(e) => setProduct_Id(e.target.value)}
+                    >
+                      {products?.map((prod, index) => (
+                        <option key={index} value={prod._id}>{prod.Product_Name}</option>
+                      ))}
+                    </select> 
+                </div>
+                <div className="mt-2">
+                 <label htmlFor="" className="text-gray-800 text-lg block">Date</label>
+                 <input type="date" 
+                     placeholder="Enter Product Name" 
+                     className="w-full px-3 bg-sky-100 py-2 p-1 rounded-full mt-1 focus:outline-1 focus:outline-sky-300"
+                     onChange={(e) => setDate(e.target.value)} />
+                </div>
+                
+                <div className="mt-2">
+                 <label htmlFor="" className="text-gray-800 text-lg block">Qauntity</label>
+                 <input type="number" 
+                     placeholder="Enter Product Quantity" 
+                     className="w-full px-3 bg-sky-100 py-2 p-1 rounded-full mt-1 focus:outline-1 focus:outline-sky-300"
+                     onChange={(e) => setQuantity(e.target.value)} />
+                </div>
+
+                <button onClick={handleAddStockOut} className="w-full flex justify-center items-center  mt-6 bg-sky-300 py-2 gap-3 text-white font-bold rounded-full hover:bg-sky-400 transition-colors"><FaPlus />Add</button>
+            </div>
+        </div>
+    )
+}
+
+export default AddStockOut;
