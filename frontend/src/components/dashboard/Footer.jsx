bf5981f58995526e7d943884869392a0e811c42a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 10:58:50 +0200

    First commit of EPSA

--- frontend/src/components/dashboard/Footer.jsx
+++ frontend/src/components/dashboard/Footer.jsx
@@ -1,5 +1,5 @@
 import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa"
-import { FaHome,FaChartBar, FaBox, FaArrowDown, FaArrowUp, FaSignInAlt, FaUserPlus } from "react-icons/fa";
+import { FaHome,FaChartBar, FaBox, FaArrowDown, FaArrowUp, FaTelegram } from "react-icons/fa";
 
 import { Link } from "react-router-dom";
 const Footer = () => {
@@ -8,7 +8,7 @@ const Footer = () => {
             <div className="flex justify-between">
                <div className="bg-linear-to-l from-white to-sky-800 bg-clip-text text-transparent">
                     <p className="mt-3 text-clip text-transparent font-bold  text-3xl  transition duration-200">Saint Anne</p>
-                    <p>Saint App is app that help to manage products in the kitchen.</p>
+                    <p className="text-white mt-3 font-bold">Saint App is app that help to manage products in the kitchen.</p>
                 </div>
                  <div className="mt-4">
                     <h1 className="text-xl text-white font-bold border-s-3 me-2 border-green-500">Qick Links</h1>
@@ -21,19 +21,25 @@ const Footer = () => {
                 </nav>
                </div>
 
-               <div>
-                 <h1>Contact</h1>
-                 <p>+250 728 184 299</p>
+               <div className="mt-4">
+                 <h1 className="text-xl text-white font-bold border-s-3 me-2 border-purple-500">Contact</h1>
+                 <p className="font-bold text-white mt-3">+250 728 184 299</p>
+                 <p className="font-bold text-white mt-3">niyomugaboetiene53@gmail.com</p>
                </div>
 
-               <div>
-                <h1>Social</h1>
-                <div>
-                <FaFacebook />
-                <FaInstagram />
-                <FaYoutube />
+               <div className="mt-4 me-5 p-3">
+                <h1  className="text-xl text-white font-bold border-s-3 me-2 border-blue-500">Social</h1>
+                <div className="grid text-white grid-cols-2 mt-3 space-x-4 space-y-5">
+                <p className="bg-blue-500 flex justify-center items-center p-3 rounded-lg hover:scale-110 hover:translate-y-3 transition duration-200" title="Facebook"><FaFacebook /></p>
+                <p className="bg-purple-500 flex justify-center items-center p-3 rounded-lg hover:scale-110 hover:translate-y-3 transition duration-200" title="Instagram"><FaInstagram /></p>
+                <p className="bg-red-500 flex justify-center items-center p-3 rounded-lg hover:scale-110 hover:translate-y-3 transition duration-200" title="Youtube"><FaYoutube /></p>
+                <p className="bg-sky-500 flex justify-center items-center p-3 rounded-lg h-10 w-10 hover:scale-110 hover:translate-y-3 transition duration-200" title="Telegram"><FaTelegram /> </p>
                 </div>
                </div>
+            </div>
+            <hr className="text-gray-500 -mt-0 "/>
+            <div className="mt-2">
+                <p className="text-white text-center -mt-1 font-bold">&copy; 2026. All right reserved</p>
             </div>
         </div>
     )
