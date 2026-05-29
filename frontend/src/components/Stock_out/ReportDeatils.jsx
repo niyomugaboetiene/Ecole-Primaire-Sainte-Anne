37f624d30721df4777c5523fa1e08c2338830483
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 08:48:33 +0200

    First commit of EPSA

--- frontend/src/components/Stock_out/ReportDeatils.jsx
+++ frontend/src/components/Stock_out/ReportDeatils.jsx
@@ -24,11 +24,13 @@ const ReportDeatils = () => {
     }, [Product_Id]);
 
     return (
-        <div>
-            <div>
-                <h1>Product Deatils</h1>
+        <div className="bg-sky-100 min-h-screen flex justify-center items-center">
+            <div className="w-125 h-125 bg-white rounded-lg shadow-lg">
+                <div className="bg-linear-to-br p-2 rounded-lg from-sky-400 to-sky-600 via-sky-300">
+                   <h1 className="text-sky-700 text-xl font-bold mb-3">Product Deatils</h1>
+                   <h1 className="text-md text-sky-700 font-bold">{details?.stockIn[0].Product_Id?.Product_Name}</h1>
+                </div>
                 <div>
-                   <h1>{details?.stockIn[0].Product_Id?.Product_Name}</h1>
                    <p>Product Id{details?.stockIn[0].Product_Id?.Product_Id}</p>
                    <p>Stock In{details?.totalStockIN}</p>
                    <p>Stock Out{details?.totalStockOut}</p>
