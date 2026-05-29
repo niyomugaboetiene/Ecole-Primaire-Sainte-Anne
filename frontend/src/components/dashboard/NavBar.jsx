fa416230b2f7fc09d5e72fa7a95e4b3e25e48aa7
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 09:16:35 +0200

    First commit of EPSA

--- frontend/src/components/dashboard/NavBar.jsx
+++ frontend/src/components/dashboard/NavBar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div className="bg-sky-400">
-            <div className="flex">
+        <div className="bg-sky-400 p-5 h-25">
+            <div className="flex justify-between">
                 <div>
                     Logo
                 </div>
-               <div>
-                   <nav>
+               <div >
+                   <nav className="flex">
                       <Link><FaHome />Home</Link>
                       <Link><FaBox />Products</Link>
                       <Link><FaArrowDown />Stock In</Link>
@@ -18,7 +18,7 @@ const NavBar = () => {
                 </nav>
                </div>
                <div>
-                   
+                   Admin Data
                </div>
             </div>
         </div>
