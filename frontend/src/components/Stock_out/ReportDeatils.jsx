75cd46e281eba02ec6f54288e4df19ba2a72fd0e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 09:04:40 +0200

    First commit of EPSA

--- frontend/src/components/Stock_out/ReportDeatils.jsx
+++ frontend/src/components/Stock_out/ReportDeatils.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
-import {FaQuit, FaNew} from "react-icons/fa"
+import { FaArrowLeft, FaPlus} from "react-icons/fa"
 
 const ReportDeatils = () => {
     const { Product_Id } = useParams();
@@ -42,8 +42,8 @@ const ReportDeatils = () => {
                    <p className="bg-sky-300 p-2 py-3 rounded-full">{details?.remainingStock}</p>
 
                    <div className="flex justify-between mt-5 ">
-                    <button className="w-55 bg-linear-to-br text-white font-bold rounded-lg  py-2 from-red-600 hover:scale-105 transition duration-300 to-red-400"><Fa />Back</button>
-                    <button  className="w-55 bg-linear-to-br text-white font-bold rounded-lg  py-2 from-green-600 hover:scale-105 transition duration-300 to-green-400">Find New</button>
+                    <button className="w-55 bg-linear-to-br text-white font-bold rounded-lg  py-2 from-red-600 hover:scale-105 transition duration-300 to-red-400 flex items-center justify-center gap-2"><FaArrowLeft />Back</button>
+                    <button  className="w-55 bg-linear-to-br text-white font-bold rounded-lg  py-2 from-green-600 hover:scale-105 transition duration-300 to-green-400 flex items-center justify-center gap-2 h-12"><FaPlus />Find New</button>
                    </div>
                  </div>
             </div>
