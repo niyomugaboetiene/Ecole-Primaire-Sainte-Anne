935a8292d51f3464c83de323988feea37d2f707a
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 10:41:59 +0200

    First commit of EPSA

---
+++ frontend/src/components/dashboard/Footer.jsx
@@ -0,0 +1,38 @@
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa"
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+               <div className="bg-linear-to-l from-white to-sky-800 bg-clip-text text-transparent">
+                    <p className="mt-3 text-clip text-transparent font-bold  text-3xl  hover:scale-105 transition duration-200">Saint Anne</p>
+                </div>
+                               <div >
+                   <nav className="flex space-x-4 mt-4">
+                      <Link className="flex gap-2 hover:text-gray-800 hover:font-bold transition duration-300 text-white font-bold"><FaHome className="mt-1"/>Home</Link>
+                      <Link className="flex gap-2 hover:text-gray-800 hover:font-bold transition duration-300 text-white font-bold" to={'/products/list'}><FaBox className="mt-1" />Products</Link>
+                      <Link className="flex gap-2 hover:text-gray-800 hover:font-bold transition duration-300 text-white font-bold" to={'/stockIn/list'}><FaArrowDown  className="mt-1"/>Stock In</Link>
+                      <Link className="flex gap-2 hover:text-gray-800 hover:font-bold transition duration-300 text-white font-bold" to={'/stockOut/list'}><FaArrowUp className="mt-1"/>Stock Out</Link>
+                      <Link className="flex gap-2 hover:text-gray-800 hover:font-bold transition duration-300 text-white font-bold" to={'/report'}><FaChartBar className="mt-1"/>Report</Link>
+                </nav>
+               </div>
+
+               <div>
+                 <h1>Contact</h1>
+                 <p>+250 728 184 299</p>
+               </div>
+
+               <div>
+                <h1>Social</h1>
+                <div>
+                <FaFacebook />
+                <FaInstagram />
+                <FaYoutube />
+                </div>
+               </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
