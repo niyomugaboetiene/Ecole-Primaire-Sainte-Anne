2fd9b7e052bafe70bb52c17edc5de7741738b7bf
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 10:07:35 +0200

    First commit of EPSA

--- frontend/src/components/Stock_out/AddStockOut.jsx
+++ frontend/src/components/Stock_out/AddStockOut.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { FaPlus } from "react-icons/fa";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { FaSignInAlt } from "react-icons/fa";
 
 const AddStockOut = () => {
     // Product_Id, Date, Quantity, Unit_price
@@ -11,6 +12,8 @@ const AddStockOut = () => {
     const [message, setMessage] = useState("");
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
+
+    const [isAuthorized, setIsAuthorized] = useState(true);
 
     const navigate = useNavigate();
     
@@ -23,6 +26,10 @@ const AddStockOut = () => {
            setProducts(res.data.list);
        } catch (err) {
         console.error(err);
+        const errorMessage = err.response?.data.message || "Error occured";
+        if (errorMessage === "Login first.") {
+            setIsAuthorized(false);
+        }
        }
     }
 
@@ -43,11 +50,25 @@ const AddStockOut = () => {
         } catch (err) {
             console.error(err);
             const errorMessage = err.response?.data?.message || "Error occured";
+            if (errorMessage === "Login first.") {
+               setIsAuthorized(false);
+           }
             setError(errorMessage);
             setMessage("");
         }
     }
 
+    if (!isAuthorized) {
+        return (
+            <div className="flex justify-center items-center min-h-screen">
+                <div className="bg-yellow-200 p-3 h-fit rounded-lg">
+                   <h1 className="text-center text-2xl font-bold text-yellow-700 mb-4">Security Alert</h1>
+                   <p className="text-yellow-900">You are not authorized. do to this you can login first of all.</p>
+                   <button className="bg-sky-400 flex justify-center items-center gap-2 w-1/2 mt-3 py-3 rounded-full ms-25 text-white font-bold hover:bg-sky-600" onClick={() =>navigate('/auth/login')}><FaSignInAlt /> Login</button>
+                </div>
+            </div>
+        )
+    }
     return (
         <div className="bg-sky-200 min-h-screen flex justify-center items-center ">
             <div className="bg-white p-3 h-fit w-100 rounded-lg shadow-lg">
