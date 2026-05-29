95d6caadd5f1cd7915ca5f95b0b83b9b9e1416ab
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 09:27:27 +0200

    First commit of EPSA

--- frontend/src/components/dashboard/NavBar.jsx
+++ frontend/src/components/dashboard/NavBar.jsx
@@ -5,16 +5,16 @@ const NavBar = () => {
     return (
         <div className="bg-sky-400 p-5 h-25">
             <div className="flex justify-between">
-                <div>
-                    Logo
+                <div className="bg-linear-to-l from-white to-sky-800 bg-clip-text text-transparent">
+                    <p className="mt-3 text-clip text-transparent font-bold  text-3xl  hover:scale-105 transition duration-200">Saint Anne</p>
                 </div>
                <div >
-                   <nav className="flex">
-                      <Link><FaHome />Home</Link>
-                      <Link><FaBox />Products</Link>
-                      <Link><FaArrowDown />Stock In</Link>
-                      <Link><FaArrowUp />Stock Out</Link>
-                      <Link><FaInfoCircle />About Us</Link>
+                   <nav className="flex space-x-4 mt-4">
+                      <Link className="flex gap-2 hover:text-gray-800 hover:font-bold transition duration-300 text-white font-bold "><FaHome className="mt-1"/>Home</Link>
+                      <Link className="flex gap-2 hover:text-gray-800 hover:font-bold transition duration-300 text-white font-bold"><FaBox className="mt-1" />Products</Link>
+                      <Link className="flex gap-2 hover:text-gray-800 hover:font-bold transition duration-300 text-white font-bold"><FaArrowDown  className="mt-1"/>Stock In</Link>
+                      <Link className="flex gap-2 hover:text-gray-800 hover:font-bold transition duration-300 text-white font-bold"><FaArrowUp className="mt-1"/>Stock Out</Link>
+                      <Link className="flex gap-2 hover:text-gray-800 hover:font-bold transition duration-300 text-white font-bold"><FaInfoCircle className="mt-1"/>About Us</Link>
                 </nav>
                </div>
                <div>
