494c730980f7f0bbfb05c1de0486f15be93b0e28
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2026-05-29 09:00:28 +0200

    First commit of EPSA

--- frontend/src/components/Stock_out/ReportDeatils.jsx
+++ frontend/src/components/Stock_out/ReportDeatils.jsx
@@ -30,14 +30,20 @@ const ReportDeatils = () => {
                    <h1 className="text-sky-700 text-xl font-bold mb-3">Product Deatils</h1>
                    <h1 className="text-md text-sky-700 font-bold">{details?.stockIn[0].Product_Id?.Product_Name}</h1>
                 </div>
-                <div className="bg-linear-to-b from-sky-200 to-sky-300 w-full h-full p-3 rounded-lg">
-                   <p className="text-sm font-bold text-gray-700">Product Id</p>
+                <div className="bg-linear-to-b from-sky-200 to-sky-300 w-full h-full p-3">
+                   <p className="text-sm font-bold text-gray-700  mt-3 mb-2">Product Id</p>
                    <p className="bg-sky-300 p-2 py-3 rounded-full">{details?.stockIn[0].Product_Id?.Product_Id}</p>
-                   <p className="text-sm font-bold text-gray-700">Stock In</p>
+                   <p className="text-sm font-bold text-gray-700 mt-3 mb-2">Stock In</p>
                    <p className="bg-sky-300 p-2 py-3 rounded-full">{details?.totalStockIN}</p>
-                   <p className="text-sm font-bold text-gray-700">Stock Out</p>
+                   <p className="text-sm font-bold text-gray-700 mt-3 mb-2">Stock Out</p>
                    <p className="bg-sky-300 p-2 py-3 rounded-full">{details?.totalStockOut}</p>
-                  <p>Remaining Stock{details?.remainingStock}</p>
+                   <p className="text-sm font-bold text-gray-700 mt-3 mb-2">Remaining Stock</p>
+                   <p className="bg-sky-300 p-2 py-3 rounded-full">{details?.remainingStock}</p>
+
+                   <div className="flex justify-between mt-5 ">
+                    <button className="w-55 bg-linear-to-br text-white font-bold rounded-lg  py-2 from-red-600 hover:scale-105 transition duration-300 to-red-400">Back</button>
+                    <button  className="w-55 bg-linear-to-br text-white font-bold rounded-lg  py-2 from-green-600 hover:scale-105 transition duration-300 to-green-400">Find New</button>
+                   </div>
                  </div>
             </div>
         </div>
